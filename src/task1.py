079a17dbb315f9a3cd90b0c84e1d9f8fcf021bc9
cherylthy <59245154+cherylthy@users.noreply.github.com>
2024-04-07 14:03:09 +0800

    updated task 1

--- src/task1.py
+++ src/task1.py
@@ -4,12 +4,12 @@ import os
 import sys
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import regexp_replace
+from pyspark.sql.functions import regexp_replace, udf
 
 from utils import load_tweets, convert_data_type
 
 def select_columns(dataframe):
-    selected_columns = ["_channel", "_trust", "_country", "airline", "airline_sentiment", "text", "airline_sentiment_gold", "negativereason"]
+    selected_columns = ["_channel", "_trust", "_country", "airline", "airline_sentiment", "text", "airline_sentiment_gold", "negativereason", "tweet_coord", "tweet_location"]
     
     return dataframe.select(selected_columns)
 
@@ -18,6 +18,11 @@ def remove_duplicates(dataframe):
     deduplicated_tweets = dataframe.dropDuplicates()
 
     return deduplicated_tweets
+
+def ascii_ignore(df_text):
+    return df_text.encode('ascii', 'ignore').decode('ascii')
+
+ascii_udf = udf(ascii_ignore)
 
 def clean_text(dataframe, column_name):
     # Remove punctuation
@@ -29,6 +34,17 @@ def clean_text(dataframe, column_name):
     # Remove any urls found in the text
     dataframe = dataframe.withColumn(column_name, regexp_replace(column_name, r'\b\w*http\w*\b', ''))
 
+    # Remove any UDF
+    dataframe = dataframe.withColumn(column_name, ascii_udf(column_name))
+
+    # Remove emoticons like :D or :p
+    dataframe = dataframe.withColumn(column_name, regexp_replace(column_name, r":[\w]+\b", ''))
+
+    # Remove any non-alphanumeric numbers
+    dataframe = dataframe.withColumn(column_name, regexp_replace(column_name, '[^a-zA-Z0-9\s]', ''))
+
+
+
     return dataframe
 
 
@@ -38,7 +54,7 @@ def run():
     os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
     spark = SparkSession.builder.appName("task1").getOrCreate()
-    tweets = load_tweets(spark, "../data/Twitter_Airline Dataset")
+    tweets = load_tweets(spark, "INF2006_TwitterAnalysis/data/twitter_airline")
 
     # remove duplicates
     cleaned_tweets = remove_duplicates(tweets)
@@ -52,12 +68,13 @@ def run():
     # convert to correct data type
     cleaned_tweets = convert_data_type(cleaned_tweets, "_trust", "float")
 
+
     # print schema to check if data type is correct
     cleaned_tweets.printSchema()
     
     print(cleaned_tweets.show(5))
 
-    cleaned_tweets.coalesce(1).write.csv(path="../results/task1", mode="overwrite")
+    cleaned_tweets.coalesce(1).write.csv(path="INF2006_TwitterAnalysis/results/task1", mode="overwrite", header=True)
 
 
 if __name__ == "__main__":
